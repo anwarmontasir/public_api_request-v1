5207ecb79c016f6b5a0064b26ddeb297471c34b6
anwarmontasir <anwarmontasir@gmail.com>
2021-05-25 16:51:00 -0700

    working on date of birth

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,8 @@ function fetchData(url) {
 
 fetchData('https://randomuser.me/api/?results=12')
 .then(data => {
-    appendUsers(data.results);
+    // appendUsers(data.results);
+    gatherUserData(data.results);
 })
 
 function checkStatus(res) {
@@ -20,6 +21,24 @@ function checkStatus(res) {
     } else {
         return Promise.reject(new Error(res.statusText));
     }
+}
+
+function gatherUserData(results) {
+    results.forEach(result => {
+        console.log('dob', result.dob.date);
+        const user = {
+            name: `${result.name.first} ${result.name.last}`,
+            email: result.email,
+            location: `${result.location.city}, ${result.location.state}, ${result.location.country}`,
+            image: {
+                medium: result.picture.medium,
+                large: result.picture.large
+            },
+            cell: result.cell,
+           // birthday: `${result.dob.getMonth()} ${result.dob.getDay()}, ${result.dob.getFullYear()}`
+        }
+        console.log(user);
+    });
 }
 
 function appendUsers(results) {
